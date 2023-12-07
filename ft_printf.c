c1c2f0de62acfaa925190c37c7edd67a4ec108f2
Jeong-su Shin <jeshin@c6r7s4.42seoul.kr>
2023-12-07 17:17:30 +0900

    Added put_hex,put_addr in libft

--- ft_printf.c
+++ ft_printf.c
@@ -6,44 +6,11 @@
 /*   By: jeshin <jeshin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 17:01:07 by jeshin            #+#    #+#             */
-/*   Updated: 2023/12/04 21:54:45 by jeshin           ###   ########.fr       */
+/*   Updated: 2023/12/07 17:16:06 by jeshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-void	ft_puthex_fd(long int n, int fd,int upper)
-{
-	long int			l;
-	char				*set;
-
-	l = (long int)n;
-	if(upper)
-		set ="0123456789ABCDEF";
-	else
-		set = "0123456789abcdef";
-	if (l < 0)
-	{
-		l *= -1;
-		write(fd, "-", 1);
-	}
-	if (l < 16)
-		write(fd, &set[l], 1);
-	else if(l >= 16)
-	{
-		ft_puthex_fd(l / 16, 1, upper);
-		write(fd, &set[l % 16], 1);
-	}
-}
-
-void	ft_putaddr_fd(void *addr, int fd)
-{
-	unsigned long int			l;
-
-	l = (unsigned long int)addr;
-	write(fd,"0x",2);
-	ft_puthex_fd(l,fd,0);
-}
 
 int print_format(const char **str,va_list *ap){
 	int	err_is;
@@ -74,27 +41,6 @@ int print_format(const char **str,va_list *ap){
 		return (0);
 	return (1);
 }
-
-// int	check_format(va_list *ap,int type, int base, int case)
-// {
-// 	if(type == 0 && base == 0 && case == 0)
-// 		ft_putchar_fd(va_arg(*ap,char),1);//c
-// 	else if(type == 0 && base == 0 && case == 1)
-// 		ft_putstr(*ap);//s
-// 	else if(base == 10 && case == 0)
-// 		ft_putnbr(*ap);//d
-// 	else if(base == 10 && case == 0)
-// 		ft_putnbr(*ap);//i
-// 	else if(base == 16 && case == 0)
-// 		ft_putnbr(*ap);//p
-// 	else if(base == 10 && case == 0)
-// 		ft_putnbr(*ap);//u
-// 	else if(base == 16 && case == 0)
-// 		ft_putstr(ap);//x
-// 	else if(base == 16 && case == 1)
-// 		ft_putstr(ap);//X
-// 	return (1);
-// }
 
 int	ft_printf(const char *str, ...)
 {
